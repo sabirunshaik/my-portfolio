86d993e299c6311bc55de37db1a7fc66c4b800f5
sabirunshaik <149999029+sabirunshaik@users.noreply.github.com>
2024-01-31 12:36:04 +0530

    small changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     if (element) {
       init(element, {
         showCursor: true,
-        strings: ["Name", "React Developer", "Frontend Developer"],
+        strings: ["Sabirun shaik", "React Developer", "Frontend Developer"],
       });
     }
   }, []);
